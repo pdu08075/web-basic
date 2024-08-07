654797d3065472de59a9a7ff7bdc0f2862332c94
pdu08075 <pdu08075>
2024-08-07 17:02:18 +0900

    2024.08.07.16

---
+++ HTML+CSS/senicare/sign-up.js
@@ -0,0 +1,6 @@
+var userNameElement = document.getElementById('user-name');
+var userIdElement = document.getElementById('user-id');
+var userPasswordElement = document.getElementById('user-password');
+var userPasswordCheckElement = document.getElementById('user-password-check');
+var userTelnumberElement = document.getElementById('user-telnumber');
+var userAuthNumberElement = document.getElementById('user-auth-number');
