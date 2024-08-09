af08b65f8ea89e6e2c957be9cae1f1189ffbb7a4
pdu08075 <pdu08075>
2024-08-09 12:56:06 +0900

    2024.08.09.12

---
+++ TS/1.typescript/sample.ts
@@ -0,0 +1,3 @@
+const greet = 'Hello TS';
+console.log(greet);
+
--- TS/2.type/type.js
+++ TS/2.type/type.js
@@ -1,19 +1,16 @@
 /*
-    타입선언 : 
+    타입선언 :
     - 변수 및 함수에 타입을 지정하여 타입에 대한 제한을 추가할 수 있음
 */
-let variable: string;
+var variable;
 variable = '문자열';
 // variable = 1234;
-
-function func (arg1: string, arg2: number): string {
+function func(arg1, arg2) {
     return '문자열';
 }
-
-const arrowFunc = (arg1: string, arg2: number): string => {
+var arrowFunc = function (arg1, arg2) {
     return '문자열';
-}
-
+};
 /*
     typescript의 타입
     - string: 문자열
@@ -27,73 +24,64 @@ const arrowFunc = (arg1: string, arg2: number): string => {
     - void: 함수에서 반환값이 없음
     - any: 모든 타입
 */
-const numbers1: number[] = [1, 2, 3];
-const numbers2: Array<number> = [1, 2, 3];
-
-let string1: string = '';
-let string2: String = '';
+var numbers1 = [1, 2, 3];
+var numbers2 = [1, 2, 3];
+var string1 = '';
+var string2 = '';
 string2 = string1;
 // string1 = string2;
-
-enum Color {
-    RED = '#ff0000', GREEN = '#00ff00', BLUE = '#0000ff'
-}
-const color: Color = Color.RED;
+var Color;
+(function (Color) {
+    Color["RED"] = "#ff0000";
+    Color["GREEN"] = "#00ff00";
+    Color["BLUE"] = "#0000ff";
+})(Color || (Color = {}));
+var color = Color.RED;
 console.log(color);
-
-let anyType: any = 10;
+var anyType = 10;
 anyType = '10';
-
-const voidFunc = (): void => {
+var voidFunc = function () {
     return;
-}
-
-const func2 = (arg: string): string => {
+};
+var func2 = function (arg) {
     return '';
-}
-
-let funcType: (arg: string) => string;
+};
+var funcType;
 funcType = func2;
 // funcType = voidFunc;
-
 /*
     타입추론 :
-    - 타입을 선언하지 않고 변수에 값이 초기화되는 순간에 초기화된 값에 따라서 변수의 타입이 결정 
+    - 타입을 선언하지 않고 변수에 값이 초기화되는 순간에 초기화된 값에 따라서 변수의 타입이 결정
 */
-let stringVariable = '';
+var stringVariable = '';
 // stringVariable = 10;
-
-const func3 = (arg: number) => {
-    if (arg === 1) return 1;
+var func3 = function (arg) {
+    if (arg === 1)
+        return 1;
     return '';
-}
-
+};
 /*
     타입 변환 :
     - as 키워드로 형변환 가능 (객체일때 자주 사용), <> 연산자로 형변환 가능
 */
-const PI: any = '3.14';
-let piNumber: number = PI as number;
-piNumber = <number>PI;
-
+var PI = '3.14';
+var piNumber = PI;
+piNumber = PI;
 piNumber = Number(PI);
-
-
 /*
     유니온 타입
     - 복수의 타입을 지정하는 방법, | 사용
 */
-let union: number | string;
+var union;
 union = 10;
 union = '10';
 // union = false;
 // const number1: number = union as number;
-
 /*
     리터럴 타입
     - 지정한 리터럴 값만 가질수 있는 타입
 */
-let day: '낮' | '밤';
+var day;
 day = '낮';
 day = '밤';
 // day = '아침';
